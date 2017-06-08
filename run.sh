3d02bfa0db89ec0095e779d87e62a91a6d0d2cf5
Steven Berlanga <zabawaba99@gmail.com>
2017-06-07 18:12:58 -0700

    adding more memory configurations

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash -ex
 
-MAX_MEM=${MAX_MEMORY:-1G}
+MAX_HEAP=${MAX_HEAP_MEMORY:-1G}
+MAX_STACK=${MAX_STACK_SIZE:-512K}
+MAX_METASPACE=${MAX_METASPACE_MEMORY:-256M}
 
 java \
-  -Xms${MAX_MEM} \
+  -Xmx${MAX_HEAP} \
+  -Xss${MAX_STACK} \
+  -XX:MaxMetaspaceSize=${MAX_METASPACE} \
   -Djava.library.path=./DynamoDBLocal_lib \
   -jar DynamoDBLocal.jar \
   -sharedDb \
