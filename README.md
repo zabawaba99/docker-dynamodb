a65b7947bfcda0fab3b7d31d9f860571a2cfc1b0
Brennan Chapman <bchapman@magicleap.com>
2017-09-15 11:02:30 -0400

    Add in memory env var

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-dynamodb
 A docker image for dynamodb
+
+## Environment Variables
+Set the `IN_MEMORY` env var to use an in-memory cluster instead of disk based.
--- run.sh
+++ run.sh
@@ -4,13 +4,25 @@ MAX_HEAP=${MAX_HEAP_MEMORY:-1G}
 MAX_STACK=${MAX_STACK_SIZE:-512K}
 MAX_METASPACE=${MAX_METASPACE_MEMORY:-256M}
 
-java \
-  -Xmx${MAX_HEAP} \
-  -Xss${MAX_STACK} \
-  -XX:MaxMetaspaceSize=${MAX_METASPACE} \
-  -Djava.library.path=./DynamoDBLocal_lib \
-  -jar DynamoDBLocal.jar \
-  -sharedDb \
-  -dbPath \
-  /var/dynamodb_local \
-  -port 8000
+if [ -z "$IN_MEMORY" ]; then
+  java \
+    -Xmx${MAX_HEAP} \
+    -Xss${MAX_STACK} \
+    -XX:MaxMetaspaceSize=${MAX_METASPACE} \
+    -Djava.library.path=./DynamoDBLocal_lib \
+    -jar DynamoDBLocal.jar \
+    -sharedDb \
+    -dbPath \
+    /var/dynamodb_local \
+    -port 8000
+else
+  java \
+    -Xmx${MAX_HEAP} \
+    -Xss${MAX_STACK} \
+    -XX:MaxMetaspaceSize=${MAX_METASPACE} \
+    -Djava.library.path=./DynamoDBLocal_lib \
+    -jar DynamoDBLocal.jar \
+    -inMemory \
+    -sharedDb \
+    -port 8000
+fi
